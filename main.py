c7bea729e8e7f494d40a1614903cc9f388a2440c
rajesh782762 <124059761+rajesh782762@users.noreply.github.com>
2023-02-08 21:13:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Entered Project")
+from pyspark import SparkSession
+sc = SparkSession.builder.getOrCreate().sparkContext
+words = sc.parallelize (
+   ["scala", 
+   "java", 
+   "hadoop", 
+   "spark", 
+   "akka",
+   "spark vs hadoop", 
+   "pyspark",
+   "pyspark and spark"]
+)
+words_map = words.map(lambda x: (x, 1))
+mapping = words_map.collect()
+print "Key value pair -> %s" % (mapping)
